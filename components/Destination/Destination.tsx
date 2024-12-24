cb8bcbc52d9624548cad484f730d3ef36edb0377
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-12-24 16:27:54 +0300

    hero section and recommend section have already finished

---
+++ components/Destination/Destination.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SectionHeading from '../Helper/SectionHeading'
+import DestinationSlider from './DestinationSlider'
+
+const Destination = () => {
+  return (
+      <div className='pt-20 pb-20'>
+          {/* Section Heading */}
+          <SectionHeading heading="Exploring Popular Destination" />
+          {/* Section Content */}
+          <div className='mt-14 w-[80%] mx-auto'>
+              <DestinationSlider/>
+          </div>
+    </div>
+  )
+}
+
+export default Destination
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import SearchBox from '../Helper/SearchBox'
+import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative w-full h-[120vh] sm:h-[100vh]'>
+      {/* overlay  */}
+      <div className='absolute top-0 left-0 size-full bg-gray-800 opacity-70'></div>
+      <video src="/images/hero1.mp4" muted autoPlay loop preload='metadata' className='size-full object-cover' />
+      {/* text content */}
+      <div className='absolute z-[100] size-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <div className='flex items-center justify-center flex-col size-full'>
+          <div>
+            <h1 className='text-[25px] mb-4 md:mb-0 text-center 
+            md:text-[35px] lg:text-[45px] tracking-[0.7rem]
+            text-white font-bold uppercase'>
+              Lets Enjoy The Nature
+            </h1>
+            <p className='md:text-base text-center text-lg text-white font-normal [word-spacing:5px]'>
+              Get the best prices on 2.000.000 properties, worldwide
+            </p>
+          </div>
+          {/* Search Box */}
+          <SearchBox />
+          <Link href={'#'} className='rounded px-14 md:px-28 -mt-4 py-2.5 
+            overflow-hidden group bg-rose-600 relative hover:bg-gradient-to-r
+             hover:from-red-100 hover:to-red-400 text-white  hover:ring-2
+             hover:ring-offset-2 hover:ring-red-400 transition-all ease-out duration-300'>
+          <span className='absolute right-0 size-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-4 ease'></span>
+          <span className='relative font-bold'>Search</span>
+            
+             </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
